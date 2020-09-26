642b3ecc6fe0ae231eb4fa495c1e3b10a791c83b
SantaKaus <44451907+SantaKaus@users.noreply.github.com>
2020-09-26 01:06:46 -0400

    Update keybindings.rkt

--- keybindings.rkt
+++ keybindings.rkt
@@ -11,6 +11,43 @@
 (require drracket/tool-lib srfi/2)
 (require (for-syntax racket/list))
 
+(define-syntax-rule (define-shortcut-internal (key ...) name proc)
+  (begin
+    (define (name ed evt . rest)
+      (when (is-a? ed racket:text<%>)
+        (send ed begin-edit-sequence)
+        (apply proc ed evt rest)
+        (send ed end-edit-sequence)))
+    (keybinding key name) ...))
+
+(define-syntax (define-shortcut stx)
+  ;; Add esc; equivalent key bindings for all the meta bindings.
+  (define (add-esc-and-option-key-bindings s-keys)
+    (define keys (syntax->datum s-keys))
+    (define esc-variants
+      (for/list ([k (in-list keys)]
+                 #:when (regexp-match? #rx"m:" k))
+        (string-append "esc;" (regexp-replace* #rx"m:" k ""))))
+    (define option-variants
+      (for/list ([k (in-list keys)]
+                 #:when (regexp-match? #rx"m:" k))
+        (string-append "?:a:" (regexp-replace* #rx"m:" k ""))))
+    ;; Use remove-duplicates to combine all key bindings, so that duplicates
+    ;; are removed. This means that if we add some esc; key bindings manually,
+    ;; for example by accident, it will not be duplicated, affecting display
+    ;; of key bindings in DrRacket.
+    (remove-duplicates (append esc-variants option-variants keys)))
+  (syntax-case stx ()
+    [(_ key (name . args) body* ...)
+     #'(define-shortcut key name
+         (λ args body* ...))]
+    [(_ (key ...) name proc)
+     #`(define-shortcut-internal
+         (#,@(add-esc-and-option-key-bindings #'(key ...)))
+         name proc)]
+    [(_ key name proc)
+     #'(define-shortcut (key) name proc)]))
+
 (define (menu-bind key menu-item)
   (keybinding
    key
